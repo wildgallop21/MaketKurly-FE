634f2f507d1ed530979ce524c9be87efa3703c25
wildgallop21 <wildgallop21@gmail.com>
2022-09-10 18:58:31 +0900

    헤더, 푸터 아이콘 포함1차완성

--- kirlyproject/src/App.js
+++ kirlyproject/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
--- kirlyproject/src/components/Header.jsx
+++ kirlyproject/src/components/Header.jsx
@@ -12,38 +12,72 @@ import { faApple } from "@fortawesome/free-brands-svg-icons";
 const Header = () => {
   return (
     <div>
-      <HeaderContainer>
-        <Box>
-          <Logocontainer>
-            <Logo>
-              <img
-                style={{ width: "82px" }}
-                src="https://res.kurly.com/images/marketkurly/logo/logo_x2.png"
-              />
-            </Logo>
-            <LogoName>마켓컬리</LogoName>
-          </Logocontainer>
-          <SearchBox>
-            <Search></Search>
-            <SearchLogo>돋보기</SearchLogo>
-          </SearchBox>
+      <Container>
+        <HeaderTop>
+          <HeaderTop1>회원가입</HeaderTop1>
+          <HeaderTop2>로그인</HeaderTop2>
+          <HeaderTop3>고객센터</HeaderTop3>
+        </HeaderTop>
+        <HeaderContainer>
+          <Box>
+            <Logocontainer>
+              <Logo>
+                <img
+                  style={{ width: "82px" }}
+                  src="https://res.kurly.com/images/marketkurly/logo/logo_x2.png"
+                />
+              </Logo>
+              <LogoName>마켓컬리</LogoName>
+            </Logocontainer>
+            <SearchBox>
+              <Search></Search>
+              <SearchLogo></SearchLogo>
+            </SearchBox>
 
-          <IconBox>
-            <IconLocation></IconLocation>
-            <IconHeart>하트</IconHeart>
-            <IconBasket>장바구니</IconBasket>
-            {/* <FontAwesomeIcon icon="fa-solid fa-location-dot" />
+            <IconBox>
+              <IconLocation></IconLocation>
+              <IconHeart></IconHeart>
+              <IconBasket></IconBasket>
+              {/* <FontAwesomeIcon icon="fa-solid fa-location-dot" />
             <FontAwesomeIcon icon={faApple} /> */}
-          </IconBox>
+            </IconBox>
 
-          {/* <TextPurple>샛별/낮</TextPurple>
+            {/* <TextPurple>샛별/낮</TextPurple>
         <Text>배송안내</Text> */}
-        </Box>
-      </HeaderContainer>
+          </Box>
+        </HeaderContainer>
+        <HeaderBottom>
+          <HeaderBottom1>카테고리</HeaderBottom1>
+          <HeaderBottom2>
+            <HB1>신상품</HB1>
+            <HB2>베스트</HB2>
+            <HB3>알뜰쇼핑</HB3>
+            <HB4>특가혜택</HB4>
+          </HeaderBottom2>
+          <HeaderBottom3>샛별 낮 배송안내</HeaderBottom3>
+        </HeaderBottom>
+      </Container>
     </div>
   );
 };
-
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const HeaderTop = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  margin: 10px 20px 10px 0;
+`;
+const HeaderTop1 = styled.div`
+  margin: 0 10px 0px 10px;
+`;
+const HeaderTop2 = styled.div`
+  margin: 0 10px 0px 10px;
+`;
+const HeaderTop3 = styled.div`
+  margin: 0 10px 0px 10px;
+`;
 const HeaderContainer = styled.div`
   margin: 0 300px 0 300px;
   /* background-color: red; */
@@ -57,22 +91,29 @@ const Text = styled.div`
 `;
 const SearchBox = styled.div`
   display: flex;
-  flex-direction:row;
+  flex-direction: row;
   width: 400px;
   height: 48px;
   border: solid 1px #5f0080;
+  /* margin : 0 0 0 200px; */
   border-radius: 6px;
-
 
   /* background-color: blue */
 `;
-const SearchLogo = styled.div``;
+const SearchLogo = styled.div`
+position: relative;
+    width: 30px;
+    height: 30px;
+    margin: 10px;
+    bottom: 3px;
+    background: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzYiIGhlaWdodD0iMzYiIHZpZXdCb3g9IjAgMCAzNiAzNiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxnIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+CiAgICAgICAgPHBhdGggZmlsbD0ibm9uZSIgZD0iTTAgMGgzNnYzNkgweiIvPgogICAgICAgIDxnIHN0cm9rZT0iIzVGMDA4MCIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2Utd2lkdGg9IjEuNyI+CiAgICAgICAgICAgIDxwYXRoIGQ9Im0yNi4wODEgMjYuMDgxLTQuMTItNC4xMk0xNi40NjcgMjMuMzM0YTYuODY3IDYuODY3IDAgMSAwIDAtMTMuNzM0IDYuODY3IDYuODY3IDAgMCAwIDAgMTMuNzM0eiIvPgogICAgICAgIDwvZz4KICAgIDwvZz4KPC9zdmc+Cg==) 0px 0px no-repeat;
+`;
 
 const Search = styled.input`
   margin: 5px 0 5px 0;
   width: 350px;
   height: 28px;
-  border:none;
+  border: none;
   /* background-color: skyblue; */
   font-size: 16px;
   letter-spacing: -0.33px;
@@ -105,6 +146,7 @@ const Logocontainer = styled.div`
   width: 300px;
   height: 63px;
   /* background-color: yellow; */
+  /* margin: 30px 0 0 0 ; */
   display: flex;
   flex-direction: row;
 `;
@@ -115,24 +157,23 @@ const IconBox = styled.div`
   flex-direction: row;
 `;
 const IconLocation = styled.div`
-  background-color: green;
-  margin: 0px 20px 0px 0px;
-  width: 36px;
-  height: 36px;
-  background: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzYiIGhlaWdodD0iMzYiIHZpZXdCb3g9IjAgMCAzNiAzNiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxnIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+CiAgICAgICAgPHBhdGggZD0iTTM2IDM2SDBWMGgzNnoiLz4KICAgICAgICA8ZyB0cmFuc2Zvcm09InRyYW5zbGF0ZSg4LjcgNikiIHN0cm9rZT0iI2VlZSIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgc3Ryb2tlLXdpZHRoPSIxLjciPgogICAgICAgICAgICA8cGF0aCBkPSJNOS4zMDYgMjRTMCAxNi41NDQgMCA5LjMwNmE5LjMwNiA5LjMwNiAwIDAgMSAxOC42MTIgMEMxOC42MTIgMTYuNTQ0IDkuMzA2IDI0IDkuMzA2IDI0eiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIi8+CiAgICAgICAgICAgIDxjaXJjbGUgc3Ryb2tlLWxpbmVjYXA9InNxdWFyZSIgY3g9IjkuMjEyIiBjeT0iOS4xMjMiIHI9IjIuNzg0Ii8+CiAgICAgICAgPC9nPgogICAgPC9nPgo8L3N2Zz4K)
-    50% 50% no-repeat;
+    width: 36px;
+    height: 36px;
+    background: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzYiIGhlaWdodD0iMzYiIHZpZXdCb3g9IjAgMCAzNiAzNiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxnIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+CiAgICAgICAgPHBhdGggZD0iTTM2IDM2SDBWMGgzNnoiLz4KICAgICAgICA8ZyB0cmFuc2Zvcm09InRyYW5zbGF0ZSg4LjcgNikiIHN0cm9rZT0iIzMzMyIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgc3Ryb2tlLXdpZHRoPSIxLjciPgogICAgICAgICAgICA8cGF0aCBkPSJNOS4zMDYgMjRTMCAxNi41NDQgMCA5LjMwNmE5LjMwNiA5LjMwNiAwIDAgMSAxOC42MTIgMEMxOC42MTIgMTYuNTQ0IDkuMzA2IDI0IDkuMzA2IDI0eiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIi8+CiAgICAgICAgICAgIDxjaXJjbGUgc3Ryb2tlLWxpbmVjYXA9InNxdWFyZSIgY3g9IjkuMjEyIiBjeT0iOS4xMjMiIHI9IjIuNzg0Ii8+CiAgICAgICAgPC9nPgogICAgPC9nPgo8L3N2Zz4K) 50% 50% no-repeat;
+    cursor: pointer;
 `;
 const IconHeart = styled.div`
-  background-color: green;
-  margin: 0px 20px 0px 0px;
-  width: 36px;
-  height: 36px;
+width: 36px;
+    height: 36px;
+    background: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzYiIGhlaWdodD0iMzYiIHZpZXdCb3g9IjAgMCAzNiAzNiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxwYXRoIGQ9Ik0yOC45MjcgOC44OTNhNi40NiA2LjQ2IDAgMCAwLTkuMTM5IDBsLTEuODI5IDEuODI4LTEuODI3LTEuODI4YTYuNDYyIDYuNDYyIDAgMSAwLTkuMTQgOS4xMzhMOC44MiAxOS44Nmw4LjQzMiA4LjQzNGExIDEgMCAwIDAgMS40MTQgMGw4LjQzMy04LjQzNGgwbDEuODI4LTEuODI4YTYuNDYgNi40NiAwIDAgMCAwLTkuMTM4eiIgc3Ryb2tlPSIjMzMzIiBzdHJva2Utd2lkdGg9IjEuNyIgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIiBzdHJva2UtbGluZWNhcD0icm91bmQiLz4KPC9zdmc+Cg==) 50% 50% no-repeat;
+    cursor: pointer;
 `;
 const IconBasket = styled.div`
-  background-color: green;
-  margin: 0px 20px 0px 0px;
-  width: 36px;
-  height: 36px;
+display: block;
+    width: 36px;
+    height: 36px;
+    background: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzYiIGhlaWdodD0iMzYiIHZpZXdCb3g9IjAgMCAzNiAzNiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxnIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+CiAgICAgICAgPHBhdGggZD0iTTM2IDM2SDBWMGgzNnoiLz4KICAgICAgICA8ZyB0cmFuc2Zvcm09InRyYW5zbGF0ZSg1LjE2NCA2LjU0NykiIHN0cm9rZT0iIzMzMyIgc3Ryb2tlLWxpbmVjYXA9InNxdWFyZSIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgc3Ryb2tlLXdpZHRoPSIxLjciPgogICAgICAgICAgICA8cGF0aCBkPSJtMjUuNjggMy42Ni0yLjcyIDExLjU3SDcuMzdMNC42NiAzLjY2eiIvPgogICAgICAgICAgICA8Y2lyY2xlIGN4PSIyMC41MiIgY3k9IjIwLjc4IiByPSIyLjE0Ii8+CiAgICAgICAgICAgIDxjaXJjbGUgY3g9IjkuODEiIGN5PSIyMC43OCIgcj0iMi4xNCIvPgogICAgICAgICAgICA8cGF0aCBkPSJNMCAwaDMuOGwxLjc2IDcuNSIvPgogICAgICAgIDwvZz4KICAgIDwvZz4KPC9zdmc+Cg==);
+    cursor: pointer;
 `;
 
 const Box = styled.div`
@@ -145,5 +186,69 @@ const Box = styled.div`
   flex-direction: row;
   /* justify-content: space-evenly; */
 `;
+const HeaderBottom = styled.div`
+  margin: 0 0 0 10%;
+  display: flex;
+  flex-direction: row;
+
+`;
+const HeaderBottom1 = styled.div`
+  margin: 0 10% 0 10%;
+  width: 150px;
+    height: 55px;
+    height: fit-content;
+    font-size: 25px;
+    font-weight: 500;
+    color: #333;
+`;
+
+const HeaderBottom2 = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 0 0 0 0;
+  `;
+
+const HeaderBottom3 = styled.div`
+  margin: 0 10% 0 10%;
+  width: 100px;
+    height: 55px;
+    height: fit-content;
+    font-size: 15px;
+    font-weight: 500;
+    color: #333;
+  `;
+
+const HB1 = styled.div`
+    width: 150px;
+    height: 55px;
+    height: fit-content;
+    font-size: 25px;
+    font-weight: 1000;
+    color: #333;
+    `;
+const HB2 = styled.div`
+    width: 150px;
+    height: 55px;
+    height: fit-content;
+    font-size: 25px;
+    font-weight: 500;
+    color: #333;
+`;
+const HB3 = styled.div`
+    width: 150px;
+    height: 55px;
+    height: fit-content;
+    font-size: 25px;
+    font-weight: 500;
+    color: #333;
+    `;
+const HB4 = styled.div`
+    width: 150px;
+    height: 55px;
+    height: fit-content;
+    font-size: 25px;
+    font-weight: 500;
+    color: #333;
+    `;
 
 export default Header;
