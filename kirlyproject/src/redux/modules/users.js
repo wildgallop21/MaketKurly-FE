1ddb814f4699af68acd7bae1b582083cdc3c2e0e
suzz-in <o_o_su_jin_@naver.com>
2022-09-13 16:57:02 +0900

    회원가입 css수정

--- kirlyproject/src/redux/modules/users.js
+++ kirlyproject/src/redux/modules/users.js
@@ -1,7 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-
 const API_USERS_URL = "http://localhost:3001/users";
 
 const initialState = {
@@ -9,14 +8,14 @@ const initialState = {
     {
       member_id: "",
       password: "",
-      name:"",
-      email:"",
-      phone_number:"",
+      name: "",
+      email: "",
+      phone_number: "",
       address: "",
-      gender:"",
-      birth:"",
-      policy:""
-    }
+      gender: "",
+      birth: "",
+      policy: "",
+    },
   ],
   isLoading: false,
   error: null,
@@ -26,9 +25,12 @@ export const createUserThunk = createAsyncThunk(
   "users/createUserThunk",
   async (newUser, thunkAPI) => {
     try {
-      const data = await axios.post("http://localhost:3001/users", newUser);
-      // const data = await axios.post("http://13.125.246.47:8080/api/member/signup", newUser);
-      console.log(data)
+      // const data = await axios.post("http://localhost:3001/users", newUser);
+      const data = await axios.post(
+        "http://52.79.58.138/member/signup",
+        newUser
+      );
+      console.log(data);
       return thunkAPI.fulfillWithValue(data);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
@@ -41,26 +43,23 @@ export const loginUserThunk = createAsyncThunk(
   async (userInfo, thunk) => {
     console.log("test");
     try {
-      // const data =
-        await // axios.post('http://localhost:3001/user/login', userInfo)
-        axios
-        .post('http://localhost:3001/users', userInfo)
-          .then((response) => {
-            // if (response.data.success) {
-              localStorage.setItem(
-                "accessToken",
-                response.headers["access_token"]
-              );
-              localStorage.setItem(
-                "refreshToken",
-                response.headers["refresh_token"]
-              );
-            // } else if (response.status !== 200) {
-            //   alert("서버와 연결에 실패했습니다.");
-            // } else {
-            //   alert("이메일과 비밀번호를 확인해주세요.");
-            // }
-          });
+      await axios
+        .post("http://52.79.58.138/member/login", userInfo)
+        .then((response) => {
+          localStorage.setItem(
+            "access-token",
+            response.headers["access-token"]
+          );
+          localStorage.setItem(
+            "refresh-token",
+            response.headers["refresh-token"]
+          );
+          // } else if (response.status !== 200) {
+          //   alert("서버와 연결에 실패했습니다.");
+          // } else {
+          //   alert("이메일과 비밀번호를 확인해주세요.");
+          // }
+        });
       // console.log(data);
     } catch (error) {
       return thunk.rejectWithValue(error);
