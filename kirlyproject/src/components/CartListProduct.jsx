e750414e312c821a92df36d0872765fa9eb8178f
wildgallop21 <wildgallop21@gmail.com>
2022-09-15 12:09:39 +0900

    장바구니 상품이름, 가격  불러오기 구현. 이미지 사이즈 조절 실패

--- kirlyproject/src/components/CartListProduct.jsx
+++ kirlyproject/src/components/CartListProduct.jsx
@@ -53,8 +53,10 @@ const CartListProduct = (props) => {
             <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij4KICAgIDxnIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+CiAgICAgICAgPGc+CiAgICAgICAgICAgIDxnPgogICAgICAgICAgICAgICAgPGc+CiAgICAgICAgICAgICAgICAgICAgPGcgdHJhbnNmb3JtPSJ0cmFuc2xhdGUoLTE3Ni4wMDAwMDAsIC0xMDkwLjAwMDAwMCkgdHJhbnNsYXRlKDEwMC4wMDAwMDAsIDkzNi4wMDAwMDApIHRyYW5zbGF0ZSg2MC4wMDAwMDAsIDE0Mi4wMDAwMDApIHRyYW5zbGF0ZSgxNi4wMDAwMDAsIDEyLjAwMDAwMCkiPgogICAgICAgICAgICAgICAgICAgICAgICA8Y2lyY2xlIGN4PSIxMiIgY3k9IjEyIiByPSIxMiIgZmlsbD0iIzVGMDA4MCIvPgogICAgICAgICAgICAgICAgICAgICAgICA8cGF0aCBzdHJva2U9IiNGRkYiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgc3Ryb2tlLXdpZHRoPSIxLjUiIGQ9Ik03IDEyLjY2N0wxMC4zODUgMTYgMTggOC41Ii8+CiAgICAgICAgICAgICAgICAgICAgPC9nPgogICAgICAgICAgICAgICAgPC9nPgogICAgICAgICAgICA8L2c+CiAgICAgICAgPC9nPgogICAgPC9nPgo8L3N2Zz4K" alt="" className="css-1wfsi82 e31wziv0"/>
             </Check>
             <FoodImage>
-              음식사진
-
+              {/* <img alt="test" src={filtered[0]?.itemIMG}
+              styled={{width: "130px", height: "200px"}}>
+              </img> */}
+              
             </FoodImage>
             
             <FoodText>
@@ -71,7 +73,8 @@ const CartListProduct = (props) => {
               +
             </FoodButton>
             <FoodPrice>
-              28800
+              
+              {filtered[0]?.itemPrice}
             </FoodPrice>
             <FoodButton>
               X
